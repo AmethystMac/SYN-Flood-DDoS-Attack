52ed356131e501b4dbdafd9286f6d49980d479da
AmethystMac <matthewtrichy@gmail.com>
2022-11-19 11:31:10 -0500

    Flags in wsAPI.py
    Pyshark can now detect the packets which doesn't wait for an acknowledgement.

--- source/wsAPI.py
+++ source/wsAPI.py
@@ -1,20 +1,19 @@
 import pyshark
 
-# capture = pyshark.FileCapture("/home/kali/Desktop/wsCapture.pcapng")    # Importing the capture file
+source_ip = "172.22.8.1"
+destination_ip = "172.22.8.1"
 
-# for packet in capture:      # Iterating through the packets in the file
-#     print(packet.ip.src)    # Printing the source address of the packet
-#     print(packet.ip.dst, "\n")    # Printing the destination address of the packets
-
-source_ip = "172.22.10.40"
-destination_ip = "34.102.187.140"
+syn = 2
+ack = 10
 
 capture = pyshark.LiveCapture(interface='eth0')
 
 try:
     for packet in capture.sniff_continuously():
-        if "IP" in packet and packet.ip.src == source_ip:
-            print ("Source: ", packet.ip.src, "   Destination: ", packet.ip.dst)
+        if "IP" in packet and "TCP" in packet:
+            flags = int(str(packet.tcp.flags)[-3:])
+            if flags & syn and flags & ack:
+                print ("Source: ", packet.ip.src, "   Destination: ", packet.ip.dst)
 
-except AttributeError:
-    print("Attribute Error: Try Again")
+except AttributeError as ae:
+    print("Attribute Error:", ae)
